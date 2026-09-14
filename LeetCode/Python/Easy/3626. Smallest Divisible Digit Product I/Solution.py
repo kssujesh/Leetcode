d86a6e1d025f401f878c4faf18d90ace79f0e115
S K S <183062684+kssujesh@users.noreply.github.com>
2026-09-14 20:33:52 +0530

    Add Easy Python Solution for 3626. Smallest Divisible Digit Product I from LeetCode

---
+++ LeetCode/Python/Easy/3626. Smallest Divisible Digit Product I/Solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def smallestNumber(self, n: int, t: int) -> int:
+        while True:
+            product = 1
+            for i in str(n):
+                product *= int(i)
+
+            if product % t == 0:
+                return n
+            n += 1
+            
+            
+        
